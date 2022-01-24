0446a72f847d9930f70ba3f7e999817e9d3541fb
NikoGardziella <gardziella@gmail.com>
2022-01-24 21:02:12 +0200

    Win Ui

---
+++ ClashOfClans/Assets/gameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public string[] teams;
+    public Color[] teamColor;
+    public GameObject vicotryUi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
