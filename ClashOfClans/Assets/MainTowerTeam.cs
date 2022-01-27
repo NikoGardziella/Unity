76839a10d85cf16bb681794726259d48792d613e
NikoGardziella <gardziella@gmail.com>
2022-01-27 19:57:05 +0200

    WinCanvas

--- ClashOfClans/Assets/MainTowerTeam.cs
+++ ClashOfClans/Assets/MainTowerTeam.cs
@@ -7,13 +7,19 @@ public class MainTowerTeam : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var myinfo = gameObject.GetComponent<properties>();
-        var controller = gameObject.transform.Find("GameController").GetComponent<gameController>();
+       // var myinfo = gameObject.GetComponent<properties>();
+       //  var controller = gameObject.transform.Find("GameController").GetComponent<gameController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var myinfo = gameObject.GetComponent<properties>();
+        if (myinfo.currentHealth <= 0)
+        {
+            gameObject.transform.Find("GameController").GetComponent<gameController>();
+            destr
+   
+        }
     }
 }
