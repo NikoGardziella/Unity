a2aee425163f4bd2252f63277c2aadfdc44ab5fb
NikoGardziella <gardziella@gmail.com>
2022-03-01 21:24:15 +0200

    update

--- ClashOfClans/Assets/DialogueManager.cs
+++ ClashOfClans/Assets/DialogueManager.cs
@@ -14,29 +14,51 @@ public class DialogueManager : MonoBehaviour
     {
         sentences = new Queue<string>();
     }
+    void Update()
+    {
+
+        if (Input.anyKeyDown && animator.GetBool("isOpen") == true)
+        {
+            Debug.Log("anykeypressed");
+            DisplayNextSentence();
+        }
+
+    } 
     public void StartDialogue(dialogue dialogue)
     {
         animator.SetBool("isOpen", true);
+        
 
         Debug.Log("StartDialogue");
-        sentences.Clear();
+      //  sentences.Clear(); // should this be moved/removed?
 
         foreach(string sentence in dialogue.sentences)
 		{
             sentences.Enqueue(sentence);
+
 		}
+        
         DisplayNextSentence();
+       
     }
     public void DisplayNextSentence()
     {
         if(sentences.Count == 0)
 		{
+
             EndDialogue();
             return;
 		}
+
         string sentence = sentences.Dequeue();
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
+     /*   if (sentences.Count == 0)
+        {
+            Time.timeScale = 0;
+        } */
+
+
     }
 
     IEnumerator TypeSentence(string sentence)
@@ -47,11 +69,13 @@ public class DialogueManager : MonoBehaviour
             dialogueText.text += letter;
             yield return null;
 		}
-	}
+        
+    }
 
     void EndDialogue()
 	{
         animator.SetBool("isOpen", false);
+       // Time.timeScale = 1;
         Debug.Log("end of dialogue");
 	}
 
